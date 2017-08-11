ac791a9215d0db856286d3be10d697fb59df41a2
AlliedToasters <michael.r.klear@gmail.com>
2017-08-11 16:58:39 -0300

    added comments to datawrangler.py and analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def set_plot(pitchers, names, column, n, year):
+def set_plot(pitchers, names, column, n, year, title):
     """Takes a dictionary of dataframes as arg1, a list of names as arg2,
     a string as arg3, an integer as arg4, and an int year as arg5. Ranks
     pitchers(arg1) based on column (arg3) in year (arg5) and divides 
@@ -10,36 +10,38 @@ def set_plot(pitchers, names, column, n, year):
     line, those in list "names" as red x's, and dotted lines and group
     labels to show groups given by report.
     """
-    fig = plt.figure(figsize=(16,10))
+    fig = plt.figure(figsize=(16,10)) #big plot
     ax = fig.add_subplot(111)
-    ax.set_title('2016 Injuries')
-    ax.set_ylabel('{} in {}'.format(column, year))
+    ax.set_title(title)
+    ax.set_ylabel('{} in {}'.format(column, year)) #describe plot
     ax.set_xlabel('Player Rank from Lowest {} to Highest {} in {}'.format(column, column, year))
     length = len(pitchers)
-    group_size = length/n
-    ranked_values = []
+    group_size = length/n # to rank into n groups
+    ranked_values = [] #will contain all values for 'column'
     for pitcher in pitchers:
         df = pitchers[pitcher]
-        i = list(df[df['Year']==year].index)[0]
-        ranked_values.append(float(df.loc[i, column]))
-    ranked_values.sort()
-    plt.plot(ranked_values)
-    y_scale = max(ranked_values)/10
-    txt_loc = group_size/2
+        i = df[df['Year']==year].index #gets index for year we need
+        ranked_values.append(float(df.loc[i, column])) #gets value from column that year
+    ranked_values.sort() #sorts from lowest to highest value, 'ranks'
+    plt.plot(ranked_values) #plots in default blue ranked values, lowest to highest
+    y_scale = max(ranked_values)/10 #for text placement
+    txt_loc = group_size/2 #x coord for first group label
     for i in range(0, n):
-        y_val = ranked_values[int(group_size * i)]
-        plt.axhline(y=y_val, color='k', linestyle='dashed')
-        ax.text((txt_loc+group_size/4), y_val+(y_scale/n), 'Group {}'.format(i+1), fontsize=11, color='g')
-        txt_loc += group_size
-    rank_iter = 1
+        y_val = ranked_values[int(group_size * i)] #min y value for group 
+        plt.axhline(y=y_val, color='k', linestyle='dashed') #plots a line for group limit
+        ax.text((txt_loc+group_size/4), y_val+(y_scale/n), 'Group {}'.format(i+1), fontsize=11, color='g') #group label
+        txt_loc += group_size #reposition x coord for next group label
+    rank = 0
+    value = 0
     for pitcher in pitchers:
-        df = pitchers[pitcher]
-        i = df[df['Year']==year].index
-        if pitcher in names:
+        df = pitchers[pitcher] #get player df
+        i = df[df['Year']==year].index #index for this year
+        if pitcher in names: #plots red x at player's location along plot
                 value = float(df.loc[i, column])
                 rank = ranked_values.index(value)
                 plt.scatter(x=rank, y=value, marker='x', color='r')
-
+    plt.scatter(x=rank, y=value, marker='x', color='r', label='Player injured in 2016')
+    plt.legend(loc='upper left')
                 
 def report(pitchers, names, column, n, year):
     """Takes a dictionary of dataframes as arg1, a list of names as arg2,
@@ -49,26 +51,25 @@ def report(pitchers, names, column, n, year):
     belong in each quantile, and reports back the number in each quantile
     (called groups 1-n) as a percentage of all pitchers.
     """
-    group_size = int(len(pitchers)/n)
+    group_size = int(len(pitchers)/n) #define quantile size for n quantiles
     ranked_values = []
     for pitcher in pitchers:
         df = pitchers[pitcher]
-        i = list(df[df['Year']==year].index)[0]
-        ranked_values.append(float(df.loc[i, column]))
-    ranked_values.sort()
-    lower_bound = 0
+        j = list(df[df['Year']==year].index)[0]
+        ranked_values.append(float(df.loc[j, column]))
+    ranked_values.sort() #ranks all values by size 
+    lower_bound = 0 #bounds for first group
     upper_bound = group_size
     for i in range(0, n):
-        basket = 0
+        basket = 0 #basket is number in group i+1
         for name in names:
             df = pitchers[name]
-            ind = list(df[df['Year']==year].index)[0]
-            if ranked_values[lower_bound] <= (float(df.loc[ind, column])) < ranked_values[upper_bound-1]:
-                basket += 1
-        if i == (n-1):
-            basket += 1 #Because above for loop does not count last item
-        print('Group {}, {}% of all players, represents {}% of the group.'.format((i+1), (100/n), 
-100*(basket/len(names))))
-        lower_bound += group_size
+            ind = list(df[df['Year']==year].index)[0] #get index for year
+            if ranked_values[lower_bound] <= df.loc[ind, column] <= ranked_values[upper_bound-1]:
+                basket += 1 #if value within group bounds, add to basket
+            if i == n-1 and df.loc[ind, column] == ranked_values[upper_bound-1]:
+                basket += 1 #Because above for loop does not count if name is highest value in ranked
+        print('Group {}, {}% of all players, represents {}% of the group.'.format((i+1), (100/n), 100*(basket/len(names)))) #reports results
+        lower_bound += group_size #iterate group bounds by group size
         upper_bound += group_size
     
--- datawrangler.py
+++ datawrangler.py
@@ -1,85 +1,52 @@
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 
 def players_outs(df_in):
     """Takes a dataframe as argument and returns a dictionary,
     keys are player names, values are dataframes with columns
     'Year' and 'Outs Pitched'
     """
-    players = {}
+    players = {} #This will be returned at the end of the function.
     for row in df_in.index:
-        rowdict = dict(df_in.loc[row])
-        name = rowdict['playerID']
-        if name not in players:
+        rowdict = dict(df_in.loc[row]) #Dummy dict, keys are column names, values in corresponding row
+        name = rowdict['playerID'] 
+        if name not in players: #Checks to see if players already has this player
             df = pd.DataFrame(columns=['Year', 'Outs Pitched'], index=[1])
-            df['Year'] = rowdict['year']
-            df['Outs Pitched'] = rowdict['Outs Pitched']
-            players[name] = df
-        else:
+            # df will be value, playerID will be key in dict players
+            df['Year'] = rowdict['year'] #df has 'Year' column
+            df['Outs Pitched'] = rowdict['Outs Pitched'] #df has 'Outs Pitched' column
+            players[name] = df #assign df to key name
+        else: #in case playerID already in players
             df = pd.DataFrame(columns=['Year', 'Outs Pitched'], index=[(list(players[name].index)[-1]+1)])
-            df['Year'] = rowdict['year']
+            #creates a new dataframe with extra row
+            df['Year'] = rowdict['year'] #populate new row
             df['Outs Pitched'] = rowdict['Outs Pitched']
-            players[name] = pd.concat([players[name], df])
-    return players
-    
-def consol(df):
-    """consol takes a dataframe as its argument and returns a dataframe.
-    If argument df has multiple rows with the same year, it adds together
-    'Outs Pitched' values and gives the sum to the single row for that year.
-    """
-    count = {}
-    for row in df.index:
-        count[df.loc[row, 'Year']] = 0
-    for row in df.index:
-        count[df.loc[row, 'Year']] += df.loc[row, 'Outs Pitched']
-    result = pd.DataFrame(index=range(df.index[0], len(count)+df.index[0]))
-    result['Year'] = count.keys()
-    result['Outs Pitched'] = count.values()
-    return result
+            players[name] = pd.concat([players[name], df]) #concat old df with new row 
+    return players 
 
 def fill_gaps(df):
     """Takes a dataframe argument. If df has a gap between years (like
     2014, 2016), adds a row for the missing year with 0 outs pitched.
     """
-    last_year = int(df.iloc[0]['Year'])
-    years_to_add = []
+    last_year = int(df.iloc[0]['Year']) #start w/ first year
+    years_to_add = [] #this will track years we need to add
     for row in df.index:
         this_year = int(df.loc[row, 'Year'])
-        if (this_year - last_year) != 1:
-            gap = this_year - last_year
-            for i in range(1, gap):
-                years_to_add.append(last_year + i)
-        last_year = this_year
-    for year in years_to_add:
-        i = list(df[df['Year']==(year - 1)].index)[0]
-        new_df = pd.DataFrame(columns = df.columns, index=range(1, (len(df) + 2)))
-        years = list(df.loc[1:i, 'Year']) + [year] + list(df.loc[i+1:, 'Year'])
-        outs_pitched = list(df.loc[1:i, 'Outs Pitched']) + [0] + list(df.loc[i+1:, 'Outs Pitched'])
-        new_df['Year'] = years
+        gap = this_year - last_year
+        for i in range(1, gap): #if gap is 0 or 1, no iteration
+            years_to_add.append(last_year + i) #adds necessary year
+        last_year = this_year #advances cycle
+    for year in years_to_add: #to add year:
+        i = list(df[df['Year']==(year - 1)].index)[0] #gets index for year-1
+        new_df = pd.DataFrame(columns = df.columns, index=range(1, (len(df) + 2))) #new df w/ proper len
+        years = list(df.loc[1:i, 'Year']) + [year] + list(df.loc[i+1:, 'Year']) #insert necessary year
+        outs_pitched = list(df.loc[1:i, 'Outs Pitched']) + [0] + list(df.loc[i+1:, 'Outs Pitched']) #0 for outs pitched
+        new_df['Year'] = years #insert into new df
         new_df['Outs Pitched'] = outs_pitched
         df = new_df
     return df
         
-
-def tally_outs(df):
-    """Takes a dataframe as argument, adds up a running total for
-     total outs pitched in an entire season up until the given
-    season. Adds two new rows, one with the total number of outs
-    pitched at the beginning of the given season, one with the total
-    at the end of the season. Returns a dataframe.
-    """
-    if len(df) <= 1:
-        df.loc[1, 'Outs TD Start'] = 0
-        df.loc[1, 'Outs TD Finish'] = df.loc[1, 'Outs Pitched']
-    else:
-        for i, row in enumerate(df.index):
-            if i == 0:
-                df.loc[row, 'Outs TD Start'] = 0
-            else:
-                df.loc[row, 'Outs TD Start'] = df.loc[row-1, 'Outs Pitched'] + df.loc[row-1, 'Outs TD Start']
-                df.loc[row-1, 'Outs TD Finish'] = df.loc[row, 'Outs TD Start']
-        df.iloc[-1, 3] = df.iloc[-1, 2] + df.iloc[-1, 1]
-    return df
 
 def season_load(df):
     """Takes dataframe as argument. Calculates average outs pitched to date
@@ -89,24 +56,25 @@ def season_load(df):
     TD is outs pitched/ average outs pitched to date. Returns a dataframe with
     two new columns.
     """
-    if len(df) <= 1:
+    if len(df) <= 1: #if one season, load is 1
         df.loc[1, 'Mean Outs/Season'] = df.loc[1, 'Outs Pitched']
         df.loc[1, 'Season Load TD'] = 1
         df.loc[1, 'Season Load'] = 1
     else:
         for row in df.index:
-            if row == 1:
-                df.loc[1, 'Mean Outs/Season'] = df.loc[1, 'Outs Pitched']
-            else:
+            if row == 1: #first season, mean outs == outs pitched
+                df.loc[1, 'Mean Outs/Season'] = df.loc[1, 'Outs Pitched'] #
+            else: #is mean outs per season before starting season
                 df.loc[row, 'Mean Outs/Season'] = df.loc[row, 'Outs TD Start']/(row-1)
-            if df.loc[row, 'Mean Outs/Season'] != 0:
+            if df.loc[row, 'Mean Outs/Season'] != 0: #avoid divide by zero error
                 df.loc[row, 'Season Load TD'] = df.loc[row, 'Outs Pitched']/df.loc[row, 'Mean Outs/Season']
+            #season load TD is outs pitched/mean outs/season that year
             else:
-                df.loc[row, 'Season Load TD'] = 1
+                df.loc[row, 'Season Load TD'] = 1 #default to 1
         total_outs = df.iloc[-1]['Outs TD Finish']
         avg_outs = total_outs/len(df)
         for row in df.index:
-            if avg_outs != 0:
+            if avg_outs != 0: #season load is outs pitched over career average outs pitched/season
                 df.loc[row, 'Season Load'] = df.loc[row, 'Outs Pitched'] / avg_outs
             else:
                 df.loc[row, 'Season Load'] = 1
@@ -118,21 +86,21 @@ def format_names(name_list):
     returns a list of formatted names.
     """
     for name in name_list:
-        index = name_list.index(name)
+        index = name_list.index(name) #so we can reference name in list
         name = name.lower()
         names = name.split()
-        name1 = names[-1]
-        if "'" in name1:
-            name1 = name1[0] + name1[2:]
+        name1 = names[-1] #last name
+        if "'" in name1: #checks for names like o'leary
+            name1 = name1[0] + name1[2:] #removes second character, "'"
         if len(name1) >= 5:
-            name1 = name1[0:5]
+            name1 = name1[0:5] #first five of last name
         name2 = ''
-        if names[0][1] == '.':
-            name2 = names[0][0] + names[0][2]
+        if names[0][1] == '.': #checks for names like a.j.
+            name2 = names[0][0] + names[0][2] #change to aj
         else:
-            name2 = names[0][0:2]
-        newname = name1 + name2 + '01'
-        name_list[index] = newname
+            name2 = names[0][0:2] #first two of first name
+        newname = name1 + name2 + '01' #numeral 01 at end
+        name_list[index] = newname #replace original name
     return name_list
 
 def reconcile_names(name_list, reference_list):
@@ -142,20 +110,20 @@ def reconcile_names(name_list, reference_list):
     a list of names not found.
     """
     names_to_reconcile = list(reference_list)
-    names_not_found = []
+    names_not_found = [] #this will be the list of names that don't match reference
     for i, name in enumerate(name_list):
-        while name not in names_to_reconcile:
+        while name not in names_to_reconcile: #only if name not in list
             name_numeral = int(name[-2:])
-            name_numeral += 1
+            name_numeral += 1 #cycle through numeral
             name_numeral = str(name_numeral)
-            if len(name_numeral) == 1:
+            if len(name_numeral) == 1: #add 0 to string so like 02
                 name_numeral = '0' + name_numeral
-            if len(name_numeral) > 2:
+            if len(name_numeral) > 2: #if too long, gives up
                 name_numeral = '01'
                 name = name[:-2] + name_numeral
-                names_not_found.append(name)
-                names_to_reconcile.append(name)
-            new_name = name[:-2] + name_numeral
-            name_list[i] = new_name
-            name = new_name
+                names_not_found.append(name) #records name not in ref list
+                names_to_reconcile.append(name) #adds to this list to break loop
+            new_name = name[:-2] + name_numeral #builds new name in our format
+            name_list[i] = new_name #replaces name in list
+            name = new_name #will break while loop if correct
     return name_list, names_not_found
